f9ef53513f0dc92bbce003da0037ae37567fd820
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-01-07 19:58:42 -0700

    Building out main algorithm

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var confirmBtn = document.querySelector("#confirm");
+var confirmBtn = document.querySelector("#confirm");
+
+function checkCheckbox() {
+  var lowerChk = document.getElementById("lower");
+  if (lowerChk.checked) {
+    console.log("CHECKED");
+  } else {
+    console.log("NOT CHECKED");
+  }
+}
+
+var upperChk = document.getElementById("upper");
+var numericChk = document.getElementById("numeric");
+var specialChk = document.getElementById("special");
+
+var characterList = {
+  lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  numeric: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  special: [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", '"', '/', ']'],
+};
 
 // Write password to the #password input
-function writePassword() {
+function criteria() {
   var showCriteria = document.getElementById("criteria");
   showCriteria.classList.remove("hide");
-  document.getElementById("generate").innerHTML = "Confirm & Generate"
+  var hideButton = document.getElementById("generate");
+  hideButton.classList.add("hide");
+  return
+};
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function writePassword() {
+    checkCheckbox();
 }
 
 
+
+
+
+
+//   var password = generatePassword();
+
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+
+//   password.value = "test"
+
+// }
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", criteria);
+confirmBtn.addEventListener("click",writePassword);
+
 
